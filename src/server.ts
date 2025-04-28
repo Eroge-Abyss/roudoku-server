9009947d382438747f62af3d16e632d1cfc79a75
kofta999 <mostafaxxx555@gmail.com>
2025-04-28 14:37:11 +0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
+import { nanoid } from "nanoid";
 import type * as Party from "partykit/server";
 
 export default class Server implements Party.Server {
+  private host: string | null = null;
+
   constructor(readonly room: Party.Room) {}
+
+  static async onFetch(
+    _req: Party.Request,
+    _lobby: Party.FetchLobby,
+    _ctx: Party.ExecutionContext,
+  ) {
+    return new Response(nanoid(10));
+  }
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
     // A websocket just connected!
@@ -9,21 +20,29 @@ export default class Server implements Party.Server {
       `Connected:
   id: ${conn.id}
   room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`
+  url: ${new URL(ctx.request.url).pathname}`,
     );
+
+    if (!this.host) {
+      this.host = conn.id;
+    }
 
     // let's send a message to the connection
     conn.send("hello from server");
   }
 
   onMessage(message: string, sender: Party.Connection) {
+    if (sender.id !== this.host) {
+      return;
+    }
+
     // let's log the message
     console.log(`connection ${sender.id} sent message: ${message}`);
     // as well as broadcast it to all the other connections in the room...
     this.room.broadcast(
-      `${sender.id}: ${message}`,
+      message,
       // ...except for the connection it came from
-      [sender.id]
+      [sender.id],
     );
   }
 }
